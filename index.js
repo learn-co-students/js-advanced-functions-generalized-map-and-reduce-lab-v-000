284a3de7b8eeef0c88c894554dcce9d16f4c380b
ahotchkin <ahotchk1@yahoo.com>
2019-10-27 15:18:57 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(arr, fn) {
+  let newArr = [];
+  arr.forEach(elem => {
+    newArr.push(fn(elem));
+  });
+  return newArr;
+}
+
+function reduce(arr, fn, start) {
+  let a = !!start ? start : arr[0];
+  let i = !!start ? 0 : 1;
+  for (i; i < arr.length; i++) {
+    a = fn(a, arr[i]);
+  };
+  return a;
+}
