a581ab65ecb819a09917b47c50254a95d68ccb7e
David Forer <david.j.forer@gmail.com>
2019-11-16 09:14:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your functions here
+
+function map(array, callbackFunc) {
+    let newArray = [];
+    array.forEach(index => {
+        newArray.push(callbackFunc(index));
+    })
+    return newArray;
+}
+
+function reduce(array, func, initialValue) {
+    let i;
+    let accumulator;
+
+    if (!initialValue) {
+        accumulator = array[0];
+        i = 1;
+    } else {
+        accumulator = initialValue;
+        i = 0;
+    }
+    for (i; i < array.length; i++) {
+        accumulator = func(accumulator, array[i])
+    }
+    return accumulator;
+}
