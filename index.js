86ede061d8e3b08b9fa2fa8d1b769a82299a75df
Richard Cho <richardcho100@gmail.com>
2019-11-22 18:04:24 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(array, someFunction) {
+    let newArr = []
+    for (const element of array) {
+        newArr.push(someFunction(element))
+    }
+    return newArr
+}
+
+function reduce(array, someFunction, startingPoint) {
+    let total = !!startingPoint ? startingPoint : array[0]
+    let i = !!startingPoint ? 0 : 1
+
+    for (i; i < array.length; i++) {
+        total = someFunction(array[i], total)
+    }
+    return total
+}
+
