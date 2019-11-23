fda3abd27a4aa1fdad2fa185ddd2d11d0316d7a5
Rebecca Tong <rtong@buffalo.edu>
2019-11-23 15:59:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array,fx){
+  var newArray = []
+  var i
+  for(i=0; i < array.length; i++){
+    newArray.push(fx(array[i]))
+  }
+  return newArray
+}
+
+function reduce(array,fx){
+  var newArray = []
+  var i
+  for(i=0; i < array.length; i++){
+    newArray.push(fx(array[i]))
+  }
+  return newArray
+}
