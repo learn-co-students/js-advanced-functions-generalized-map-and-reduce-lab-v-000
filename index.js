9946a265a26689b25a669f56331f316955ba7c14
Gingertonic <bethmschofield@gmail.com>
2019-11-29 15:08:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your functions here
+
+function map(arr, fn) {
+    let newArr = []
+    for (const x of arr) { newArr.push(fn(x)) }
+    return newArr
+}
+
+function reduce(arr, fn, start) {
+    let result = start || arr[0]
+    !!start ? null : arr.shift()
+    for (const x of arr){ result = fn(x, result)}
+    return result
+}
+
