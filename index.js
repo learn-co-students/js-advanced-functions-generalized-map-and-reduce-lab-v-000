9b44cf2138f9b484363764a505bf6393c83c019a
github username <github email address>
2019-12-11 05:30:36 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Add your functions here
+function map(arr, fn) {
+  let newarr = [];
+  for (const ele of arr) {
+    let n = fn(ele);
+    newarr.push(n);
+  }
+  return newarr;
+}
+
+function reduce(arr, fn, int=0) {
+  let newval = int;
+
+  if (typeof fn(true, true) == 'boolean') {
+    newval = true;
+  }
+
+  for (const ele of arr) {
+    newval = fn(newval, ele);
+  }
+
+  return newval;
+}
