b2a8f89e78a737d6cf7c8185dcef6d065046d248
nanton83 <nicholas.romans@gmail.com>
2019-12-13 11:42:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your functions here
+function map(array, callBackFunc){
+    let new_array = []
+    for (let i = 0; i < array.length; i++) {
+        let eachElement = array[i]
+        new_array.push(callBackFunc(eachElement))
+      }
+    
+      return new_array;
+}
+
+function reduce(array, callBackFunc, startValue){
+
+    let originalValue = null;
+
+    if(startValue == 0){
+        originalValue = 0
+    } else {
+        originalValue = startValue
+    }
+        
+
+    for (let i = 0; i < array.length; i++) {
+        callBackFunc(array[i], originalValue)
+    }
+    
+}
