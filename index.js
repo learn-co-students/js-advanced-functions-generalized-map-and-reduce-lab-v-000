f456768bb03977f9332434493be0070f7c203ad1
Richard-Burd <qalbalassadalasfour@gmail.com>
2020-01-03 10:27:55 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+
+function map(originalArray, callBackFunction) {
+  let myNewArray = []
+
+  for (let i = 0; i < originalArray.length; i++) {
+    let theElement = originalArray[i]
+    myNewArray.push(callBackFunction(theElement))
+  }
+
+  return myNewArray;
+}
+
+function reduce(originalArray, callBackFunction, startPoint){
+  let output = (!!startPoint) ? startPoint : originalArray[0]
+  let i = (!!startPoint) ? 0 : 1
+
+  for (; i < originalArray.length; i++) {
+    output = callBackFunction(originalArray[i], output)
+  }
+
+  return output;
+}
