10701f7829d56d60e3f63c1c2ebf3722e1835856
Sushi Chanrai <sushi@pathworks.co>
2020-01-17 18:58:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your functions here
+
+function map(arr,fn){
+  return arr.map(x => fn(x))
+}
+
+// let arr = [1,2,3]
+// let start = 100
+
+function reduce(arr,fn, start){
+  let result = start || arr[0]
+   !!start ? null : arr.shift()
+   for (const x of arr){ result = fn(x, result)}
+   return result
+}
+
+
+
+
+
+
+//   function reduce(arr, fn, start) {
+//     let result = start || arr[0]
+//     !!start ? null : arr.shift()
+//     for (const x of arr){ result = fn(x, result)}
+//     return result
+// }
+
+
+
+// function reduceToTotal(arr, start = 0){
+//   return arr.reduce(function(start, current){
+//     return start + current
+//   }, start)
