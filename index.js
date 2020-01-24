27a84ebfcdc15b2741f56e0e4b495a287327c579
Meg Gutshall <meghan.gutshall@gmail.com>
2020-01-23 20:25:27 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Add your functions here
+function map(sourceArray, mapFunk) {
+  let newArray = [];
+  for (const el of sourceArray) {
+    newArray.push(mapFunk(el));
+  }
+  return newArray;
+};
+
+
+function reduce(src, cb, starting){
+  let r = (!!starting) ? starting : src[0]
+  let i = (!!starting) ? 0 : 1
+
+  for (; i < src.length; i++) {
+    r = cb(src[i], r)
+  }
+
+  return r;
+}
