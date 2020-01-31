75251c8db2edd7d9d0a8927e6fa0b030f43af254
lizbur10 <liz.burton147@gmail.com>
2020-01-31 21:22:35 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+function map(arr, fn) {
+    let newArray = [];
+    arr.forEach(el => {
+        newArray.push(fn(el));
+    })
+    return newArray;
+}
+
+function reduce(arr, fn, init = null) {
+    let agg = init;
+    let i;
+    if (agg) {
+        i = 0;
+    } else {
+        i = 1;
+        agg = arr[0];
+    }
+    while (i < arr.length) {
+        agg = fn(agg, arr[i]);
+        i++;
+    }
+    return agg;
+}
