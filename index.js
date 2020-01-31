972ccc03ba374856a8dd77bfc3ce3578192338e6
GiancarloZ <gzabaleta48@gmail.com>
2020-01-31 16:08:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your functions here
+function map(src, fn){
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        let j = src[i];
+        r.push(fn(j))
+    }
+    return r
+}
+
+function reduce(src, fn, start){
+    let i = !!start ? 0 : 1
+    let g = !!start ? start : src[0]
+    for (; i < src.length; i++){
+        console.log("this is i", i)
+        g = fn(src[i],g)
+    }
+    console.log("this is g", g)
+    return g
+}
