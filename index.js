aa78e322911c3dd64c74eb15cc18e272cf0e9f74
shktkt219 <shktkt219@gmail.com>
2020-02-10 22:39:10 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(original, callBack){
+   let newArray = []
+   for(let i=0; i<original.length; i++){
+       newArray.push(callBack(original[i]))
+   }
+   return newArray;
+}
+
+function reduce(original, callBack, startingPoint){ 
+    let newValue = (!!startingPoint)? startingPoint : original[0]
+    let i = (!!startingPoint)? 0 : 1
+    for(; i<original.length; i++){
+        newValue = callBack(original[i], newValue)
+    }
+    return newValue;
+}
