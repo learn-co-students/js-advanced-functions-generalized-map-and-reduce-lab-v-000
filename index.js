6a6c2a0db7d706fd0109a5a91d4477c40e1d1c8d
danetter17 <etter.dt@gmail.com>
2020-02-18 18:30:42 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your functions here
+function map(array, math) {
+  let newArray = []
+  for (let i = 0; i < array.length; i++) {
+    let arrayPosition = array[i]
+    newArray.push(math(arrayPosition))
+  }
+  return newArray;
+}
+
+
+// Ask about this solution in study group
+function reduce(array, cb, starting){
+  let r = (starting) ? starting : array[0]
+  let i = (starting) ? 0 : 1
+
+  for (; i < array.length; i++) {
+    r = cb(array[i], r)
+  }
+
+  return r;
+}
