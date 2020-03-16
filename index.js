da0246b5974cd9b1fe16d436e3ec0e56b500271c
ElvinGarcia <dimix181@gmail.com>
2020-03-16 18:12:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 
 
 
-function map(sourceArray, func) {
+function map (sourceArray, func) {
     let arr = []
     for (let i = 0; i < sourceArray.length; ++i) {
         arr.push(func(sourceArray[i]))
     }
     return arr
+}
+
+
+function reduce(sourceArray, func, startingPoint) {
+    let memo = sourceArray[0];
+    for (let i = 1; i < sourceArray.length; ++i) {
+        memo = func(sourceArray[i], memo)
+    }
+    return startingPoint ? memo += startingPoint : memo;
 }
