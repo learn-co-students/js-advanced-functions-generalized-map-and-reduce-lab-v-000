a6fe31458d6c3c42ad768a8870598c3355c66a21
Blake A. Long <blake.a.long@gmail.com>
2020-03-25 16:08:09 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+const map = (array, fn) => {
+    const new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(fn(array[i]))
+    }
+    return new_array
+}
+
+const reduce = (array, fn, startingValue) => {
+    let total 
+    if (startingValue) {
+        total = startingValue
+        for (let i = 0; i < array.length; i++) {
+            total = fn(array[i], total)
+        }
+        return total
+    } else {
+        total = array[0]
+        for (let i = 1; i < array.length; i++) {
+           total = fn(array[i], total)
+        }
+        return total
+    }
+}
+ 
