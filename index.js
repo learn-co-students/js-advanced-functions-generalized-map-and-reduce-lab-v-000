9c1663e96287404223ce480a36727e25f3ae3f40
aljimenez5 <arvida.jimenez@gmail.com>
2020-04-22 20:29:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+
+function map(arrayValues, callBack){
+    let newArray = []
+    for (let i = 0; arrayValues.length > i; i++) {
+        newArray.push(callBack(arrayValues[i]))
+    }
+    return newArray
+
+}
+
+
+function reduce(arrayValues, callBack, startPoint){
+    let total
+    let i
+    if (!!startPoint) {
+        total = startPoint
+        i = 0
+    }
+    else {
+        total = arrayValues[0]
+        i = 1
+    }
+    for (; arrayValues.length > i; i++){
+        total = callBack(arrayValues[i], total)
+        
+    }
+    return total
+}
