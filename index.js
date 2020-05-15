85cdcd6a65a3f73319c241e6d675cefeba2428d4
sdcro <sdcrouse91@gmail.com>
2020-05-14 21:22:52 -0700

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function map(sourceArray, callbackFn) {
   return newArray;
 }
 
-function reduce(sourceArray, callbackFn, startingPoint) {
+function reduce(sourceArray, callbackFn, startingPoint = 0) {
   // I don't think startingPoint needs a default value.
   // If I'm going to return true or false, then startingPoint should be initialized to true.
   // But if I'm working with numbers, then startingPoint needs to be initialized to 0, which is falsy.
@@ -18,13 +18,13 @@ function reduce(sourceArray, callbackFn, startingPoint) {
   // If that startingPoint is undefined, then it either needs to be 0 or true/truthy.
   // If it IS defined, leave it alone.
 
-  let returnValue;
+  let returnValue = startingPoint;
 
-  if (startingPoint) { // None of this will work.
-    returnValue = startingPoint;
-  } else {
-    returnValue = 0;
-  }
+  // if (startingPoint) { // None of this will work.
+  //   returnValue = startingPoint;
+  // } else {
+  //   returnValue = 0;
+  // }
 
   for (const elem of sourceArray) {
     returnValue = callbackFn(elem, returnValue);
