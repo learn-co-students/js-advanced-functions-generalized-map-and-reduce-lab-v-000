2e7f6f10cef57cbe25d7b0993c39a302ec7fae04
sjwakeman <swakeman@ca.rr.com>
2020-05-23 08:15:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,43 +1,37 @@
 // Add your functions here
+// map returns an array with all values made negative, the original values, 
+// the original values multiplied by 2, the original values squared
+let map = function(src, callback) {
+    let r = []
+    for (let i = 0; i < src.length; i++) {
+        r.push(callback(src[i])) // unique work
+    }
+            return r
+} 
 
-// map([1, 2, 3, -9], function(a){ return a * a }) 
-//function map(sourceArray, fn(sourceArray){return (-1 * sourceArray)})
-// function map(sourceArray), function(sourceArray){return (-1 * sourceArray)}
-// function map(sourceArray), function(src){return (-1 * src)}
-
-// map returns an array with all values made negative
-// function map(sourceArray) {
-//     // Set negativize to an empty array
-//     let negativize = []
-//     // iterate/loop over sourceArray
-//     for (let i = 0; i < sourceArray.length; i++) {
-//         // with all values made negative
-//         negativize.push(sourceArray[i] * -1)
-//     }
-//     // returns an array 
-//     return negativize
-// }
-
-// 
-// expect a source array and a function.
-// function map(sourceArray, function(negativize="") {
-//     function map(sourceArray, fn(negativize="") {
-// // functionUsingCallback(sayHello, sayHola, function(name="") {
-//     // All the tests will pass an Array and a block
-//     for (let i = 0; i < sourceArray.length; i++) {
-//         // iterate/loop over sourceArray
-//         // with all values made negative
-//         negativize.push(sourceArray[i] * -1)
-//     }    
-//     // returns an array 
-//      return negativize
-// })
-
-// let sayHola = function(name="") {
-let negativize = function(sourceArray) {
-    for (let i = 0; i < sourceArray.length; i++) {
-        negativize.push(sourceArray[i] * -1)
-    }   
-    return negativize
+// reduce returns a running total when not given a starting point, 
+// returns a running total when given a starting point,
+// returns true when all values are true, returns false when any value is false,
+// returns true when a true value is present andr eturns false when no true value is present.
+let reduce = function(src, callback, startingPoint) {
+    // initialize variable
+    let runningTotal 
+    // set i = 0
+    let i = 0
+    // if there is a startingPoint value
+    if (startingPoint) {
+        // set runningTotal to startingPoint
+        runningTotal = startingPoint
+    } else {
+        // If no startingPoint set stratingPoint = 0 index position
+        runningTotal = src[0]
+        // set i = 1 index poistion so we do NOT double count the 0 index position
+        i = 1
+    }
+        // Use i as the counter that is ALREADY SET in if/else statement above
+        for ( i; i < src.length; i++ ) {
+            // setting runningTotal to callback(array[index position], accumalator)
+            runningTotal = callback(src[i], runningTotal)   
+    }
+    return runningTotal
 }
-
