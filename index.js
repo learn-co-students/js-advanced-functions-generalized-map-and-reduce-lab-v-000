54f5a0ddee8f61ee7a6a83f2d1f5ce551b33d566
azharshahstl <azharshahstl@yahoo.com>
2020-06-02 16:34:22 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Add your functions here
+function map(sourceArray, callback){
+    let newArray = sourceArray.map(x => callback(x));
+    return newArray;
+}
+
+
+
+function reduce(sourceArray, callback, startingPoint){
+    let r = (!!startingPoint) ? startingPoint : sourceArray[0]
+    let i = (!!startingPoint) ? 0 : 1
+  
+    for (; i < sourceArray.length; i++) {
+      r = callback(sourceArray[i], r)
+    }
+  
+    return r;
+  }
