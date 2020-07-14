2a3acf38780a2f2ba532fa6c5727f171d41fc31b
lrh2daizzo <herrera.lrh@gmail.com>
2020-07-14 12:45:25 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(src,callBack) {
+    let r = []
+    for (let i = 0; i < src.length; i++){
+        r.push(callBack(src[i]))
+    };
+    return r;
+};
+
+function reduce(src, cb, starting){
+    let r = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      r = cb(src[i], r)
+    }
+  
+    return r;
+  }
