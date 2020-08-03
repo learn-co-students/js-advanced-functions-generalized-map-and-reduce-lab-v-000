55a30c290424c48c05bd79e247d523d220646ca7
Surpreet Mundra <surpreet.mundra@gmail.com>
2020-08-03 10:10:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map (array, action) {
+    const newArray = []
+    for (let i = 0; i < array.length; i++) {
+        newArray.push(action(array[i]))
+    }
+    return newArray
+}
+
+function reduce (sourceArray, action, starting) {
+    let r = (!!starting) ? starting : sourceArray[0]
+    let i = (!!starting) ? 0 : 1
+
+    for (; i < sourceArray.length; i++) {
+        r = action(sourceArray[i], r)
+    }
+
+    return r;
+}
