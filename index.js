33e8a90edfa4af5276daaa99ec0e808f855cf221
Liz P <parisielizabeth@gmail.com>
2020-08-14 05:12:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array, func){
+  let result = [];
+  for (let i = 0; i < array.length; i++){
+    result.push(func(array[i]))
+  }
+  return result;
+}
+
+
+function reduce(array, func, start){
+  let result = (!!start) ? start : array[0];
+  let i = (!!start) ? 0 : 1;
+  for (; i < array.length; i++){
+    result = func(array[i], result);
+  }
+  return result;
+}
