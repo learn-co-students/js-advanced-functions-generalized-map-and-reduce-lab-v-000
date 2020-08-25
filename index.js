c222be62bbe38c8a1bbb2845a1e207adfe150a1d
ADogTheWarrior <coolfudge@gmail.com>
2020-08-25 15:11:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,65 +1,15 @@
 // Your code here
-function mapToNegativize(sourceArray){
-  let newArray = []
-  sourceArray.forEach(function(value){
-    newArray.push(value * -1)
-  })
-  return newArray
-}
-function mapToNoChange(sourceArray){
-  return sourceArray
-}
-function mapToDouble(sourceArray){
-  let newArray = []
-  sourceArray.forEach(function(value){
-    newArray.push(value * 2)
-  })
-  return newArray
-}
-function mapToSquare(sourceArray){
-  let newArray = []
-  sourceArray.forEach(function(value){
-    newArray.push(value * value)
-  })
-  return newArray
-}
-
-
 function map(ary, funct){
-  return funct(ary)
-}
-
-
-
-
-
-function reduce(){
-
-}
-
-
-function reduceToTotal(sourceArray, startingPoint = 0){
-  let int = startingPoint
-  sourceArray.forEach(function(value){
-    int += value
+  let ans = []
+  ary.forEach(function(value){
+    ans.push(funct(value))
   })
-  return int
+  return ans
 }
-function reduceToAllTrue(sourceArray){
-  let returnValue = true
-  sourceArray.forEach(function(value){
-    if (value == false){
-      returnValue = false
-    }
+
+function reduce(ary, funct, start = 0){
+  ary.forEach(function(value){
+    start = funct(start, value)
   })
-  return returnValue
-}
-function reduceToAnyTrue(sourceArray){
-  let returnValue = false
-  sourceArray.forEach(function(value){
-    if (value == true){
-      returnValue = true
-    }
-  })
-  return returnValue
+  return start
 }
