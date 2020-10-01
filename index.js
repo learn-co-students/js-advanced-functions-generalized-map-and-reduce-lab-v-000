f13d0af554245f8b497d3f1e176f0bc13e39c1d0
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-01 19:18:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,38 @@
-// Add your functions here
-// 1) map returns an array with all values made negative
-// array.map([1, 2, 3, -9], function (a) { return a * a })
-
-// array.map(function (a) {
-//     return -1 * a[i];
-// }); //=> [20, 40, 60, 80]
-
-// let array = [1, 2, 3, -9]
-
-// [1, 2, 3, -9].map(function (a) {
-//      return -1 * a;
-// });
-
-// let x = map([1, 2, 3, -9], function (a) { return -1 * a })
-
-function map(src, cb) {
-    let r = []
-    for (let i = 0; i < src.length; i++) {
-        let theElement = src[i]
-        r.push(cb(theElement))
+function map(value, callback) {
+    let a = []
+    for (let i = 0; i < value.length; i++) {
+        let element = value[i]
+        a.push(callback(element))
     }
-    return r;
+    return a
 }
 
-function reduce(src, cb, starting) {
-    let r = (!!starting) ? starting : src[0]
-    let i = (!!starting) ? 0 : 1
+function reduce(value, callback, startingAmount) {
+    let a = (!!startingAmount) ? startingAmount : value[0]
+    let i = (!!startingAmount) ? 0 : 1
 
-    for (; i < src.length; i++) {
-        r = cb(src[i], r)
+    for (; i < value.length; i++) {
+        a = callback(value[i], a)
     }
-
-    return r;
+    return a
 }
+
+// function map(src, cb) {
+//     let r = []
+//     for (let i = 0; i < src.length; i++) {
+//         let theElement = src[i]
+//         r.push(cb(theElement))
+//     }
+//     return r;
+// }
+
+// function reduce(src, cb, starting) {
+//     let r = (!!starting) ? starting : src[0]
+//     let i = (!!starting) ? 0 : 1
+
+//     for (; i < src.length; i++) {
+//         r = cb(src[i], r)
+//     }
+
+//     return r;
+// }
