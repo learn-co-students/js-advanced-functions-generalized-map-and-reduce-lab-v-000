b1250d75e3f4dc86b37de119e7a33a8c2a86eb14
Amil <github email address>
2021-02-10 14:15:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(value, callback) {
+    let a = []
+    for (let i = 0; i < value.length; i++) {
+        let element = value[i]
+        a.push(callback(element))
+    }
+    return a
+}
+
+function reduce(value, callback, startingAmount) {
+    let a = (!!startingAmount) ? startingAmount : value[0]
+    let i = (!!startingAmount) ? 0 : 1
+
+    for (; i < value.length; i++) {
+        a = callback(value[i], a)
+    }
+    return a
+}
