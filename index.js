47b8f004ef047174cf81fc91fba259d6642ff233
andy-aguilar <aaguil3@gmail.com>
2021-03-08 18:12:28 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(src, cb){
+    let array = []
+    for(let i=0; i < src.length; i++){
+        array.push(cb(src[i]))
+    }
+    return array
+}
+
+function reduce(src, cb, start){
+    if(start){
+        let total = start
+        for(let i=0; i < src.length; i++){
+            total = cb(total, src[i])
+        }
+        return total
+    }
+    else{
+        let total = src[0]
+        for(let i=1; i < src.length; i++){
+            total = cb(total, src[i])
+        }
+        return total
+    }
+}
