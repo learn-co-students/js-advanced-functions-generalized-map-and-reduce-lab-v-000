28bb8ec3a85b0ddf1027da1388f79bfb5a45bac4
guysbryant <guy.s.bryant@gmail.com>
2021-05-03 19:00:39 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(a, f){
+    let arr = [];
+    for (let i = 0; i < a.length; i++){
+        let v = a[i];
+        arr.push(f(v));
+    }
+    return arr;
+}
+
+function reduce(a, f, s){
+    let v = s ? s : a[0];
+    for (let i = s ? 0 : 1; i < a.length; i++){
+        v = f(a[i], v);
+    }
+    return v;
+}
